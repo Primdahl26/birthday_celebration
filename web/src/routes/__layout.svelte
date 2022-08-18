d3572bdcfa51b2cf136d5e8e1473aa8cfca63c49
Frederik <fv@magenta-aps.dk>
2022-08-18 23:01:38 +0200

    Cleanup of login

--- web/src/routes/__layout.svelte
+++ web/src/routes/__layout.svelte
@@ -4,6 +4,13 @@
 	import Footer from '$lib/components/footer.svelte'
 </script>
 
+<svelte:head>
+	<meta
+		name="viewport"
+		content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+	/>
+</svelte:head>
+
 <div class="h-screen">
 	<Navbar />
 	<slot />
